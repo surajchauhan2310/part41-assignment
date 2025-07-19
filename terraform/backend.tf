469ed061eff5670b72ceecbf47cfb48ce03b587b
surajchauhan2310 <129864653+surajchauhan2310@users.noreply.github.com>
2025-07-19 22:24:24 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-backend-bucket-suraj2310"
-    key            = "state/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-terraform-backend-bucket-suraj2310"
+#     key            = "state/terraform.tfstate"
+#     region         = "ap-south-1"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
